481450d393b1148dd4323f5cc9f2c8af39cfbc08
Daniel <daniel.pineros@linktic.com>
2024-10-15 09:21:49 -0500

    Version 1.0
    Creacion de repositorio y proyecto principal

---
+++ src/test/java/co/uptc/frw/proyectomongo/ProyectoMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uptc.frw.proyectomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
